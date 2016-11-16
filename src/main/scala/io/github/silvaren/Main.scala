6500e0f1efa0a92e3d61bad7922b656501d50cf9
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-16 02:54:54 -0200

    Add scala basic structure

---
+++ src/main/scala/io/github/silvaren/Main.scala
@@ -0,0 +1,10 @@
+package io.github.silvaren
+
+import com.amazonaws.services.lambda.runtime.Context
+
+object MyHander extends App {
+  def handler(name: String, context: Context): String = {
+
+  }
+}
+
